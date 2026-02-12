e0145a71f45141484cf3ec4ff10a1a0d2c7a581c
Alikhan Samidinov <assamidinov2004@gmail.com>
2026-02-12 14:45:49 -0500

    Time: 15 ms (35.32%), Space: 47.2 MB (19.91%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,17 +1,27 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> magazineLetters = new HashMap<>();
+
+        for (int i = 0; i < magazine.length(); i++) {
+            char m = magazine.charAt(i);
+            
+            int currentCount = magazineLetters.getOrDefault(m, 0);
+            magazineLetters.put(m, currentCount + 1);
+        }
+
         for (int i = 0; i < ransomNote.length(); i++) {
             char r = ransomNote.charAt(i);
 
-            int matchingIndex = magazine.indexOf(r);
+            int currentCount = magazineLetters.getOrDefault(r, 0);
 
-            if (matchingIndex == -1) {
+            if (currentCount == 0) {
                 return false;
             }
 
-            magazine = magazine.substring(0, matchingIndex) + magazine.substring(matchingIndex + 1);
+            magazineLetters.put(r, currentCount - 1);
         }
 
         return true;
     }
+
 }
